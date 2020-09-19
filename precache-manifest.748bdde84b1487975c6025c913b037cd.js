cc9d13db20591b1339ade3edadec1bdb33c264b6
VENKIDESH <kvenkidesh@gmail.com>
2020-09-19 23:13:53 +0530

    Updates

---
+++ precache-manifest.748bdde84b1487975c6025c913b037cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad0650e2e709759f544424c4f6ea10b",
+    "url": "/BookStore/index.html"
+  },
+  {
+    "revision": "6809039b22b37f456db4",
+    "url": "/BookStore/static/css/2.0e1a4c73.chunk.css"
+  },
+  {
+    "revision": "8de79cf781f2725710b6",
+    "url": "/BookStore/static/css/main.8a90de5c.chunk.css"
+  },
+  {
+    "revision": "6809039b22b37f456db4",
+    "url": "/BookStore/static/js/2.320d2244.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/BookStore/static/js/2.320d2244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de79cf781f2725710b6",
+    "url": "/BookStore/static/js/main.6fe0250b.chunk.js"
+  },
+  {
+    "revision": "e43d86710f0a6b5f7ac9",
+    "url": "/BookStore/static/js/runtime-main.1de4f6c5.js"
+  },
+  {
+    "revision": "574ced7cdc6aef04df2ae6418fb58aad",
+    "url": "/BookStore/static/media/home.574ced7c.jpg"
+  },
+  {
+    "revision": "eaadf4617b5584d680526d0a4aeafc77",
+    "url": "/BookStore/static/media/logo.eaadf461.png"
+  }
+]);
